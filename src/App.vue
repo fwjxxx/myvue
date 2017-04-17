985098e11e75734fd3bac2b058491c821ae87825
fengchong <fengchong@homeba.org>
2017-04-17 18:03:45 +0800

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link >商品</router-link>
+      </div>
+      <div class="tab-item">
+          <router-link >评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link >商家</router-link>
+      </div>
+    </div>
+    <div class="content">I am content</div>
   </div>
 </template>
 
 <script>
+import header from 'components/header/header';
+
 export default {
-  name: 'app'
-}
+  components: {
+    'v-header': header
+  }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    .tab-item
+      flex: 1
+      text-align: center
+
 </style>
