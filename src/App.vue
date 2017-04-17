27f916e1c08e0ac72e8937825c8993036fa57ee8
fengchong <fengchong@homeba.org>
2017-04-17 18:06:29 +0800

    vue-1

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     <v-header></v-header>
     <div class="tab">
       <div class="tab-item">
-        <router-link >商品</router-link>
+        <router-link to="/goods">商品</router-link>
       </div>
       <div class="tab-item">
-          <router-link >评论</router-link>
+          <router-link to="/ratings">评论</router-link>
       </div>
       <div class="tab-item">
-        <router-link >商家</router-link>
+        <router-link to="/seller">商家</router-link>
       </div>
     </div>
     <div class="content">I am content</div>
