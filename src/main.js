c568e3368d70806596e51bb056e253c6a985a0a8
fengchong <fengchong@homeba.org>
2017-04-19 18:07:31 +0800

    vue-resource

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
+import VueResouce from 'vue-resource';
 import router from './router';
 
 import 'common/stylus/index.styl';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueResouce);
 
 /* eslint-disable no-new */
 new Vue({
